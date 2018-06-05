952d9db19d325b573f4fdf2626bffd8bd7cd58f9
Valeri Karpov <val@karpov.io>
2018-06-05 10:28:26 -0400

    test: repro issue with not copying numAsync

--- test/misc.test.js
+++ test/misc.test.js
@@ -9,3 +9,25 @@ describe('hasHooks', function() {
     assert.ok(!k.hasHooks('toString'));
   });
 });
+
+describe('merge', function() {
+  it('handles async pres if source doesnt have them', function() {
+    const k1 = new Kareem();
+    k1.pre('cook', true, function(next, done) {
+      execed.first = true;
+      setTimeout(
+        function() {
+          done('error!');
+        },
+        5);
+
+      next();
+    });
+
+    assert.equal(k1._pres.get('cook').numAsync, 1);
+
+    const k2 = new Kareem();
+    const k3 = k2.merge(k1);
+    assert.equal(k3._pres.get('cook').numAsync, 1);
+  });
+});
