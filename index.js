4866939d2ae18edf8f7ec68fe853d5aa26309980
Valeri Karpov <val@karpov.io>
2022-06-16 16:21:57 -0400

    fix: only pass promise resolved value to next middleware if instanceof `overwriteResult`
    Re: Automattic/mongoose#11927

--- index.js
+++ index.js
@@ -242,7 +242,12 @@ Kareem.prototype.execPost = function(name, context, args, options, callback) {
         }
 
         if (isPromiseLike(maybePromiseLike)) {
-          return maybePromiseLike.then((res) => _cb(res), err => _cb(err));
+          return maybePromiseLike.then(
+            (res) => {
+              _cb(res instanceof Kareem.overwriteResult ? res : null)
+            },
+            err => _cb(err)
+          );
         }
 
         if (maybePromiseLike instanceof Kareem.overwriteResult) {
