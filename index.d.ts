20bb65256faaf9d5ebde2c265a264946bc5824af
Valeri Karpov <val@karpov.io>
2024-03-04 12:40:02 -0500

    improve typings for options

--- index.d.ts
+++ index.d.ts
@@ -11,15 +11,15 @@ declare module "kareem" {
     clone(): Kareem;
     merge(other: Kareem, clone?: boolean): this;
 
-    createWrapper(name: string, fn: Function, context?: any, options?: any): Function;
+    createWrapper(name: string, fn: Function, context?: any, options?: Record<string, any>): Function;
     createWrapperSync(name: string, fn: Function): Function;
     hasHooks(name: string): boolean;
     filter(fn: Function): Kareem;
 
-    wrap(name: string, fn: Function, context: any, args: any[], options?: any): Function;
+    wrap(name: string, fn: Function, context: any, args: any[], options?: Record<string, any>): Function;
 
     execPostSync(name: string, context: any, args: any[]): any;
-    execPost(name: string, context: any, args: any[], options?: any, callback?: Function): void;
+    execPost(name: string, context: any, args: any[], options?: Record<string, any>, callback?: Function): void;
     execPreSync(name: string, context: any, args: any[]): any;
     execPre(name: string, context: any, args: any[], callback?: Function): void;
   }
